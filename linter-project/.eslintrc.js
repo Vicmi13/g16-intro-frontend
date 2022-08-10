b6cab2f39daa0e5ac2700be315011014e544cb3f
Victor Torres Valencia <victor.tv@wizeline.com>
2022-08-09 21:04:44 -0500

    config 'Add linter config

---
+++ linter-project/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+	"env": {
+		"browser": false, // NODE
+		"commonjs": true,
+		"es2021": true
+	},
+	"extends": "eslint:recommended",
+	"parserOptions": {
+		"ecmaVersion": "latest"
+	},
+	"rules": {
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"never"
+		]
+	}
+}
---
+++ linter-project/index.js
@@ -0,0 +1,6 @@
+const { suma } = require("./operations")
+
+const hello = () => "linter"
+
+hello()
+suma(20,49)
