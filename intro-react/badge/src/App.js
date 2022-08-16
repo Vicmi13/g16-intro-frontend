08485ad60451279e543363d18946cfaa455e6822
Francisco Campos <hey@francampos.me>
2022-08-15 18:39:27 -0600

    Badge Proyect

--- intro-react/badge/src/App.js
+++ intro-react/badge/src/App.js
@@ -1,14 +1,46 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Badge from './components/Badge';
+import BadgeForm from './components/BadgeForm';
 
-function App() {
-  return (
-    <>
-      <Navbar/>
-      <Badge firstName="Octavio" lastName="Lopez" jobTitle="Frontend" username="octavio123"/>
-    </>
-  );
+
+const props = {
+  firstName: 'Octavio',
+  lastName: 'Lopez',
+  jobTitle: 'Frontend',
+  username: 'octavio123'
+}
+
+class App extends React.Component {
+  state = {
+    form: { 
+      firstName: "",
+      lastName: ""
+    }
+  }
+
+  handleChange = e => {
+ 
+    //Modificamos el estado
+    this.setState({
+      form: { 
+        ...this.state.form,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
+  
+  render(){
+    console.log(this.state)
+    return (
+      <>
+        <Navbar/>
+        <Badge data={props} state={this.state}/>
+        <BadgeForm handleChange={this.handleChange}/>
+      </>
+    );
+  }
+  
 }
 
 export default App;
---
+++ intro-react/badge/src/components/BadgeForm.jsx
@@ -0,0 +1,28 @@
+import { Component } from 'react';
+
+class BadgeForm extends Component {
+  render() {
+    return(
+      <div>
+        <h1>Crear Card</h1>
+        <form>
+          <div className="form-group">
+            <label>Primer Nombre</label>
+            <input onChange={this.props.handleChange} type="text" className="form-control" name="firstName"/>
+          </div>
+          <div className="form-group">
+            <label>Apellido</label>
+            <input onChange={this.props.handleChange} type="text" className="form-control" name="lastName"/>
+          </div>
+          <div className="form-group">
+            <label>Username</label>
+            <input onChange={this.props.handleChange} type="text" className="form-control" name="username"/>
+          </div>
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default BadgeForm
