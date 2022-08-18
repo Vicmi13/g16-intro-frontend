f6c562ac5ba9f46bd52dd75a2c75d944ca0275be
Victor Torres Valencia <victor.tv@wizeline.com>
2022-08-17 19:40:47 -0500

    remove error

--- react-funcional/gif-app/src/components/Grid.jsx
+++ react-funcional/gif-app/src/components/Grid.jsx
@@ -1,40 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import Item from './Item'
+import React, { useEffect, useState } from "react";
+import Item from "./Item";
 
-const Grid = ({category}) => {
+const Grid = ({ category }) => {
+  const [images, setImages] = useState([]);
 
-  const [images, setImages ] = useState([])
- 
   useEffect(() => {
-    getGifs(category)
-  },[category])
+    getGifs(category);
+  }, [category]);
 
   const getGifs = async (category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=dEHnNtrWpr7gbzOKfi0yVRQdC5X8uy3g&q=${category}&limit=20`
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=dEHnNtrWpr7gbzOKfi0yVRQdC5X8uy3g&q=${category}&limit=20`;
 
-    const response = await fetch(url)
-    const { data } = await response.json()
+    const response = await fetch(url);
+    const { data } = await response.json();
 
-    const gifs = data.map(img => ({
+    const gifs = data.map((img) => ({
       id: img.id,
       title: img.title,
-      url: img.images.downsized_medium.url
-    }))
+      url: img.images.downsized_medium.url,
+    }));
 
-    setImages(gifs)
-  }
+    setImages(gifs);
+  };
 
   return (
-  <>
-  
-    <h3>{category}</h3>
-    <div className="card-grid">
-      {images.map(image => (
-        <Item key={image.id} {...image}/>
-      ))}
-     {test2}
-    </div>
-  </>)
-}
+    <>
+      <h3>{category}</h3>
+      <div className="card-grid">
+        {images.map((image) => (
+          <Item key={image.id} {...image} />
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default Grid
+export default Grid;
