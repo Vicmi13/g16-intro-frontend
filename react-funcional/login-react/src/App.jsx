d458fa5d0569b87b1b84c79b8975b551a3183f30
Victor Torres Valencia <victor.tv@wizeline.com>
2022-08-22 19:47:00 -0500

    Define states for input and password

--- react-funcional/login-react/src/App.jsx
+++ react-funcional/login-react/src/App.jsx
@@ -1,16 +1,43 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  // STEP 1 - SETEAR ESTADOS CON UN VALOR INICIAL
+  const [user, setUser] = useState('user')
+  const [password, setPassword] = useState('12345')
   
+  const userHardcoded = 'usuarioG16'
+  const passwordHardcoded = 'passw0rd'
+  // Este login harcodeado solamente será exitoso con las siguientes credenciales:
+
+  
+  // harcoded == codigo rigido || codigo duro 
+
+  // usuarioG16 passw0rd ✅
+  // usuarioG16 password 🤡
+  // usuarioG16 password1 🤡
+  // usuarioG15 passw0rd 🤡
+  // usuarioG14 passw0rd 🤡
+
+
+
+  const validateLogin = (userInput, passwordInput) => {
+
+    if(userInput === user && passwordInput === password) {
+      alert('login exitoso')
+    }
+  }
+
 
   return (
     <div className="App">
-      <input type="text" name="user" id="" />
+
+      {/* STEP 2 - ASOCIAR ESTADOS A VALOR DEL INPUT */}
+      <input type="text" value={user} name="user" id="" />
       
-      <input type="text" name="password" id="" />
+      <input type="password" value={password} name="password" id="" />
 
       <button type="submit">Ingresar</button>
     </div>
