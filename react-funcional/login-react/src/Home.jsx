bf554b729a822eded19aabf08db900d4b0673375
Victor Torres Valencia <victor.tv@wizeline.com>
2022-08-23 21:05:18 -0500

    Added conditional rendering

--- react-funcional/login-react/src/Home.jsx
+++ react-funcional/login-react/src/Home.jsx
@@ -1,28 +1,38 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import axios from 'axios'
 
 const Home = ({logout}) => {
   const fecha_actual = new Date().toLocaleDateString();
-
+  const [charactersArray, setcharactersArray] = useState([])
   /**
    *  IMPLEMENTAR LLAMADA SWAAPI CUANDO SE MONTE EL COMPONENT, UNA SOLA VEZ
    */
   
   useEffect(() => {
-    getStarWarsCharacters()
+    // getStarWarsCharacters()
+
+    (async () => {
+      try {
+        const { data: { results } } = await axios.get('https://swapi.dev/api/people')
+        setcharactersArray(results)
+      } catch(error) {
+        console.log('error', error)
+      }
+    })() // CON 👉🏽 () LA VOY A EJECUTAR 
+
     // return () => {
     //  alert('componente dismount')
     // }
   }, [])
 
-  const getStarWarsCharacters = async () => {
-    try {
-      const { data: { results } } = await axios.get('https://swapi.dev/api/people')
-      console.log('result', results)
-    } catch(error) {
-      console.log('error', error)
-    }
-  }
+  // const getStarWarsCharacters = async () => {
+  //   try {
+  //     const { data: { results } } = await axios.get('https://swapi.dev/api/people')
+  //     console.log('result', results)
+  //   } catch(error) {
+  //     console.log('error', error)
+  //   }
+  // }
   
   // se deberá ocultar este componente ????? 🤡
 
@@ -34,6 +44,16 @@ const Home = ({logout}) => {
   return (
     <div style={{ border: "3px solid #f4f0f0", width: 250, height: 300, borderLeftColor: 'orange' }}>
       Usuario loggeado en el día de 🤡 {fecha_actual}
+
+
+      {/* SINTAXIS TERNARIO condition ? true : false */}
+      {charactersArray.length === 0  ? <h4> Cargando... </h4> : 
+      charactersArray.map(objectInfo => (
+        <h3> Nombre del personaje {objectInfo.name} - altura {objectInfo.height} </h3>
+      ))
+      }
+
+      
 
       {/* STEP 1 DEFINIR EVENTO PARA CERRAR SESION */}
       <button onClick={handleLogout}> Cerrar sesión </button> 
